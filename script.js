4aacd4190e72a6b0724b18a816a9e5807462a5c4
Moa Stenqvist <moa@stonetwig.se>
2020-11-06 12:28:11 +0100

    Added functions and event for mouseover on portfolio items.

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ window.onload = start;
 function start() {
     updateTitleVisibility();
     setInterval(updateTitleVisibility, 3000);
+    addEventListeners();
+};
+
+function addEventListeners() {
+    const overlays = document.querySelectorAll('.img-overlay');
+    for (let i = 0; i < overlays.length; i++) {
+        overlays[i].onmouseover = showImgOverlay;
+        overlays[i].onmouseleave = removeImgOverlay;
+    }
+    
 };
 
 $(document).on('scroll', function() {
@@ -30,4 +40,12 @@ function moveProgressBars() {
     for (let i = 0; i < bars.length; i++) {
         bars[i].style.width = widths[i];
     }
+};
+
+function showImgOverlay(event) {
+    event.target.style.opacity = "1";
+};
+
+function removeImgOverlay(event) {
+    event.target.style.opacity = "0";
 };
